979e9fd1000858a919995099721e411649b4d348
jake-chandler <jake.chandler@uga.edu>
2022-04-16 14:31:43 -0400

    rb_tree refactor

--- src/secure_rt_core/rb_tree/rb_node.rs
+++ src/secure_rt_core/rb_tree/rb_node.rs
@@ -1,23 +1,35 @@
-#[derive(PartialEq)]
-#[derive(Copy, Clone)]
-enum Color {
+use std::cell::RefCell;
+use std::cell::RefMut;
+use std::rc::Rc;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Color {
     Red,
     Black,
 }
 
-pub struct RBNode<'a> {
-    parent: Option<&'a RBNode<'a>>,
-    l_child: Option<&'a RBNode<'a>>,
-    r_child: Option<&'a RBNode<'a>>,
-    data: u32,
-    color: Color
+#[derive(Clone)]
+pub struct RBNode {
+    pub parent: Option<Rc<RefCell<RBNode>>>,
+    pub l_child: Option<Rc<RefCell<RBNode>>>,
+    pub r_child: Option<Rc<RefCell<RBNode>>>,
+    pub data: u32,
+    pub key: u32,
+    pub color: Color,
 }
-impl PartialEq for RBNode<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.color == other.color && self.data == other.data
+
+impl RBNode {
+    pub fn from(this: Rc<RefCell<Self>>) -> Self {
+        let this = this.borrow();
+        Self {
+            parent: this.parent.clone(),
+            l_child: this.l_child.clone(),
+            r_child: this.r_child.clone(),
+            data: this.data,
+            key: this.key,
+            color: this.color,
+        }
     }
-}
-impl <'a> RBNode<'a> {
     pub fn is_root(&self) -> bool {
         match self.parent {
             Some(_) => false,
@@ -26,56 +38,52 @@ impl <'a> RBNode<'a> {
     }
 
     pub fn is_left_child(&self) -> bool {
-        match self.parent {
-            Some(parent) => {
-                match parent.l_child {
-                    Some(l_child) => self == l_child,
-                    None => false,
-                }
+        if let Some(parent) = &self.parent {
+            let parent = parent.borrow();
+            if let Some(l_child) = &parent.l_child {
+                let l_child = l_child.borrow();
+                return self.key == l_child.key;
             }
-            None => false,
         }
+        false
     }
 
     pub fn is_right_child(&self) -> bool {
-        match self.parent {
-            Some(parent) => {
-                match parent.r_child {
-                    Some(r_child) => self == r_child,
-                    None => false,
-                }
+        if let Some(parent) = &self.parent {
+            let parent = parent.borrow();
+            if let Some(r_child) = &parent.r_child {
+                let r_child = r_child.borrow();
+                return self.key == r_child.key;
             }
-            None => false,
         }
+        false
     }
 
-    pub fn grandparent(&self) -> Option<&RBNode> {
-        match self.parent {
-            Some(parent) => {
-                parent.parent
-            }
-            None => None
+    pub fn grandparent(&self) -> Option<Rc<RefCell<Self>>> {
+        if let Some(parent) = &self.parent {
+            let parent = parent.borrow();
+            return parent.parent.clone();
         }
+        None
     }
 
-    pub fn uncle(&self) -> Option<&RBNode> {
+    pub fn uncle(&self) -> Option<Rc<RefCell<Self>>> {
         match self.grandparent() {
             Some(grandparent) => {
-                if self.parent.unwrap().is_left_child() {
-                    grandparent.r_child
-                } 
-                else {
-                    grandparent.l_child
+                // self has grandparent, parent is safe to unwrap
+                if self.parent.clone().unwrap().borrow().is_left_child() {
+                    grandparent.borrow().r_child.clone()
+                } else {
+                    grandparent.borrow().l_child.clone()
                 }
-            },
-            None => None
+            }
+            None => None,
         }
     }
 
-    pub fn color_swap(&mut self, other: &mut Self) {
+    pub fn color_swap(&mut self, mut other: RefMut<Self>) {
         let temp = self.color;
         self.color = other.color;
         other.color = temp;
     }
 }
-
--- src/secure_rt_core/rb_tree/rb_tree.rs
+++ src/secure_rt_core/rb_tree/rb_tree.rs
@@ -1,5 +1,184 @@
-use super::rb_node::RBNode;
-struct RBTree<'a> {
-    root: Option<&'a RBNode<'a>>,
-    size: u32
+use super::rb_node::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+struct RBTree {
+    root: Option<Rc<RefCell<RBNode>>>,
+    size: u32,
+}
+
+impl RBTree {
+    fn rotate_left(&mut self, this: Rc<RefCell<RBNode>>) {
+        let r_child = &this.borrow().r_child;
+        let parent = &this.borrow().parent;
+        if r_child.is_none() {
+            return;
+        }
+        let r_child = r_child.as_ref().unwrap();
+        // r_child is safe to unwrap
+        this.borrow_mut().color_swap(r_child.borrow_mut());
+        // unsure if l_child is Some... clone the option
+        this.borrow_mut().r_child = r_child.borrow().l_child.clone();
+        r_child.borrow_mut().l_child = Some(this.clone());
+
+        // unsure if parent is Some... clone the option
+        r_child.borrow_mut().parent = this.borrow().parent.clone();
+        this.borrow_mut().parent = Some(r_child.clone());
+        if parent.is_none() {
+            self.root = Some(r_child.clone());
+            return;
+        }
+        // parent is safe to unwrap
+        let mut parent = parent.as_ref().unwrap().borrow_mut();
+        // is parent l_child safe to unwrap?
+        if let Some(parent_l_child) = &parent.l_child {
+            if this.borrow().key == parent_l_child.borrow().key {
+                parent.l_child = Some(r_child.clone());
+                return;
+            }
+        }
+        parent.r_child = Some(r_child.clone());
+    }
+    fn rotate_right(&mut self, this: Rc<RefCell<RBNode>>) {
+        let l_child = &this.borrow().l_child;
+        let parent = &this.borrow().parent;
+
+        if l_child.is_none() {
+            return;
+        }
+        // l_child safe to unwrap
+        let l_child = l_child.as_ref().unwrap();
+        this.borrow_mut().color_swap(l_child.borrow_mut());
+
+        // unsure if r_child is Some.. clone option
+        this.borrow_mut().l_child = l_child.borrow().r_child.clone();
+        l_child.borrow_mut().r_child = Some(this.clone());
+
+        l_child.borrow_mut().parent = this.borrow().parent.clone();
+        this.borrow_mut().parent = Some(l_child.clone());
+
+        if parent.is_none() {
+            self.root = Some(l_child.clone());
+            return;
+        }
+        let mut parent = parent.as_ref().unwrap().borrow_mut();
+        if let Some(parent_l_child) = &parent.l_child {
+            if this.borrow().key == parent_l_child.borrow().key {
+                parent.l_child = Some(l_child.clone());
+                return;
+            }
+        }
+        parent.r_child = Some(l_child.clone());
+    }
+
+    fn compare(node: Rc<RefCell<RBNode>>, key: u32) -> usize {
+        // TODO
+        0
+    }
+    fn node_search(&self, key: u32) -> (usize, Option<Rc<RefCell<RBNode>>>) {
+        let mut this = self.root.clone();
+        let mut candidate: Rc<RefCell<RBNode>>;
+        let mut result = 0;
+        loop {
+            match this {
+                None => break,
+                _ => {}
+            }
+            candidate = this.unwrap();
+            result = RBTree::compare(candidate.clone(), key);
+            let l_child = candidate.borrow().l_child.clone();
+            let r_child = candidate.borrow().r_child.clone();
+            if result != 0 {
+                if result > 0 {
+                    this = r_child;
+                } else {
+                    this = l_child;
+                }
+            } else {
+                return (result, Some(candidate));
+            }
+        }
+        (result, this)
+    }
+
+    pub fn put(&mut self, key: u32, mut this: RBNode) {
+        this.parent = None;
+        this.color = Color::Red;
+        this.l_child = None;
+        this.r_child = None;
+
+        if self.root.is_none() {
+            let node = Rc::new(RefCell::new(this));
+            node.borrow_mut().color = Color::Black;
+            self.root = Some(node);
+            return;
+        }
+        let results = self.node_search(key);
+        let node = results.1;
+        let result = results.0;
+        if result != 0 && node.is_some() {
+            this.parent = Some(node.clone().unwrap());
+            if result > 0 {
+                node.unwrap().borrow_mut().r_child = Some(Rc::new(RefCell::new(this.clone())));
+            } else {
+                node.unwrap().borrow_mut().l_child = Some(Rc::new(RefCell::new(this.clone())));
+            }
+        } else if node.is_some() {
+            // key already exists
+            let node = node.unwrap();
+            this.parent = node.borrow().parent.clone();
+            let mut node = node.borrow_mut();
+            if node.is_left_child() {
+                this.parent.unwrap().borrow_mut().l_child =
+                    Some(Rc::new(RefCell::new(this.clone())));
+            } else {
+                this.parent.unwrap().borrow_mut().r_child =
+                    Some(Rc::new(RefCell::new(this.clone())));
+            }
+            node.parent = None;
+            node.l_child = None;
+            node.r_child = None;
+            return;
+        }
+        while !this.is_root() {
+            if let Some(parent) = this.clone().parent {
+                if parent.borrow().color != Color::Red {
+                    break;
+                }
+            } else {
+                break;
+            }
+            if this.uncle().is_none() {
+                break;
+            }
+            // parent, uncle, and grandparent safe to unwrap
+            let mut uncle = this.uncle().unwrap().borrow_mut();
+            let mut parent = this.clone().parent.unwrap().borrow_mut();
+            let mut grandparent = parent.parent.clone().unwrap().borrow_mut();
+            if uncle.color == Color::Red {
+                uncle.color = Color::Black;
+                parent.color = Color::Black;
+                grandparent.color = Color::Red;
+                let grandparent = parent.parent.as_ref().unwrap();
+                this = RBNode::from(grandparent.clone());
+                continue
+            }
+            if parent.is_left_child() && this.is_left_child() {
+                let grandparent = parent.parent.as_ref().unwrap();
+                self.rotate_right(grandparent.clone());
+            } else if parent.is_left_child() && this.is_right_child() {
+                let parent = this.parent.as_ref().unwrap();
+                self.rotate_left(parent.clone());
+                self.rotate_right(parent.clone());
+            } else if parent.is_right_child() && this.is_right_child() {
+                let grandparent = parent.parent.as_ref().unwrap();
+                self.rotate_left(grandparent.clone());
+            } else {
+                let parent = this.parent.as_ref().unwrap();
+                self.rotate_right(parent.clone());
+                self.rotate_left(parent.clone());
+            }
+        }
+        self.root.as_ref().unwrap().borrow_mut().color = Color::Black;
+    }
 }
